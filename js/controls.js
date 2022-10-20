ac823c9662991eb860fa60b73bc019947e5b7708
Tiago Mota <tiagomota265@gmail.com>
2022-10-20 16:56:57 -0300

    first

---
+++ js/controls.js
@@ -0,0 +1,7 @@
+export function resetControls() {
+    buttonPlay.classList.remove('hide');
+    buttonPause.classList.add('hide');
+    buttonSet.classList.add('hide');
+    buttonStop.classList.remove('hide');
+}
+export default resetControls
