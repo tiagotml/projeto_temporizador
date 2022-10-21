8ca0cd774c356d16e494bd67463b85f83ce6f18e
Tiago Mota <tiagomota265@gmail.com>
2022-10-20 21:33:53 -0300

    refatorando

--- js/controls.js
+++ js/controls.js
@@ -1,7 +1,43 @@
-export function resetControls() {
-    buttonPlay.classList.remove('hide');
-    buttonPause.classList.add('hide');
-    buttonSet.classList.add('hide');
-    buttonStop.classList.remove('hide');
+export function Controls({
+    buttonPlay,
+    buttonPause,
+    buttonStop,
+    buttonSet,
+}) {
+    function play() {
+        buttonPlay.classList.add('hide')
+        buttonPause.classList.remove('hide')
+        buttonSet.classList.add('hide')
+        buttonStop.classList.remove('hide')
+    }
+    function pause() {
+        buttonPause.classList.add('hide')
+        buttonPlay.classList.remove('hide')
+    }
+
+    function reset() {
+        buttonPlay.classList.remove('hide');
+        buttonPause.classList.add('hide');
+        buttonStop.classList.add('hide');
+        buttonSet.classList.remove('hide');
+
+    }
+    function getMinutes() {
+        let newMinutes = prompt('Quantos minutos?')
+        if (!newMinutes) {
+            
+            return false
+        }
+
+        return newMinutes
+    }
+    
+    return {
+        reset,
+        play,
+        pause,
+        getMinutes,
+    }
 }
-export default resetControls
+
+
